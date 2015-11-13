5eef6295ad56d50d7cf1c14e644e18889e7d7507
Rene Pihlak <pihlakre@greeny.cs.tlu.ee>
2015-11-13 10:44:30 +0200

    Added previous files

---
+++ User.class.php
@@ -0,0 +1,170 @@
+<?php 
+class User {
+	
+	
+	private $connection;
+
+	//funktioon, mis käivitub siis, kui on ! NEW User();
+	function __construct($mysqli){
+	//selle klassi muutuja	
+	$this->connection = $mysqli;
+		
+		
+		
+		
+	}
+	
+	function createUser($create_email, $password_hash){
+		
+		//kas selline email on juba olemas
+		$response = new StdClass();
+		
+		$stmt = $this->connection->prepare("SELECT email FROM user_sample WHERE email = ?");
+		$stmt->bind_param("s",$create_email);
+		$stmt->execute();
+		
+		if($stmt->fetch()){
+			
+			//Saadan tagasi errori
+			$error = new StdClass();
+			$error->id = 0;
+			$error->message = "Sellinse e-mailiga kasutaja juba olemas!";
+			
+			//panen errori responsile külge
+			$response->error = $error;
+			
+			
+			return $response;
+			
+		}
+		
+		//******************
+		//***OLULINE********
+		//******************
+		
+		//panen eelimse k2su kinni
+		$stmt->close();
+		
+		$stmt = $this->connection->prepare("INSERT INTO user_sample (email, password) VALUE (?, ?)");
+				
+				//asendame ? muutujate v22rtustega
+				
+				//echo $mysqli->error;
+				//echo $stmt->error;
+				
+				$stmt->bind_param("ss",$create_email, $password_hash);
+				
+				
+				if($stmt->execute()){
+					//edukalt salvestatud
+					$success = new StdClass();
+					$success->message = "kasutaja Edukalt loodud";
+					
+					$response->success = $success;
+				}else{
+					//midagi l2ks katki
+					$error = new StdClass();
+					$error->id = 1;
+					$error->message = "Midagi l2ks Katki!";
+					
+					$response->error = $error;
+			
+					
+					
+				}
+				
+				$stmt->close();
+				
+				//saadan tagasi vastuse, kas Sucess või Error
+				return $response;
+				
+				
+		
+		
+	}
+	
+	
+	
+		function loginUser($email, $password_hash){
+			
+			
+				$response = new StdClass();
+				
+				$stmt = $this->connection->prepare("SELECT email FROM user_sample WHERE email = ?");
+				$stmt->bind_param("s",$email);
+				$stmt->execute();
+				
+				if(!$stmt->fetch()){
+					
+					//Saadan tagasi errori
+					$error = new StdClass();
+					$error->id = 2;
+					$error->message = "Sellinse e-mailiga kasutaja ei eksisteeri!";
+					
+					//panen errori responsile külge
+					$response->error = $error;
+					
+					
+					return $response;
+					
+				}
+				
+				//*******************
+				//******OLULINE******
+				//*******************
+				
+				//panen eelimse k2su kinni
+				$stmt->close();			
+				
+			
+			
+			
+				$stmt = $this->connection->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+				$stmt->bind_param("ss", $email, $password_hash); //asnendab küsimärgid
+				
+				//paneme vastused muutujatesse
+				
+				$stmt->bind_result($id_from_db, $email_from_db);
+				$stmt->execute();
+				if($stmt->fetch()){
+					//edukalt salvestatud
+					$success = new StdClass();
+					$success->message = "Sai sisse";
+					
+					$user = new StdClass();
+					$user->id = $id_from_db;
+					$user->email = $email_from_db;
+					
+					$success->user = $user;
+					
+					$response->success = $success;
+					
+
+				}else{
+					//midagi l2ks katki
+					$error = new StdClass();
+					$error->id = 5;
+					$error->message = "Midagi l2ks Katki!";
+					
+					$response->error = $error;
+			
+					
+					
+				}
+				
+				$stmt->close();
+				
+				//saadan tagasi vastuse, kas Sucess või Error
+				return $response;
+				
+		
+		
+	}
+	
+	
+	
+
+
+
+	
+} ?>
---
+++ login_sample.php
@@ -0,0 +1,152 @@
+<?php
+
+	// ühenduse loomiseks kasuta
+	require_once("functions.php");
+	
+	//Kontrollin kas kasutaja on sisse loginud
+	if(isset($_SESSION["id_from_db"])){
+		header("Location: data.php");
+		
+		
+		
+	}
+	
+
+
+  // muuutujad errorite jaoks
+	$email_error = "";
+	$password_error = "";
+	$create_email_error = "";
+	$create_password_error = "";
+  // muutujad väärtuste jaoks
+	$email = "";
+	$password = "";
+	$create_email = "";
+	$create_password = "";
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // *********************
+    // **** LOGI SISSE *****
+    // *********************
+		if(isset($_POST["login"])){
+			if ( empty($_POST["email"]) ) {
+				$email_error = "See väli on kohustuslik";
+			}else{
+        // puhastame muutuja võimalikest üleliigsetest sümbolitest
+				$email = cleanInput($_POST["email"]);
+			}
+			if ( empty($_POST["password"]) ) {
+				$password_error = "See väli on kohustuslik";
+			}else{
+				$password = cleanInput($_POST["password"]);
+			}
+      // Kui oleme siia jõudnud, võime kasutaja sisse logida
+			if($password_error == "" && $email_error == ""){
+				
+				
+				$password_hash = hash("sha512", $password);
+				//User klassi sees olev funtsioon
+				$kek_response = $User->loginUser($email, $password_hash);
+				
+				
+				if(isset($kek_response->success)){
+					
+					//var_dump($kek_response);
+					//l2ks edukalt
+					
+					$_SESSION["id_from_db"] = $kek_response->success->user->id;
+					$_SESSION["user_email"] = $kek_response->success->user->email;
+					
+					header("Location: data.php");
+				
+				//******************
+				//***OLULINE********
+				//******************
+				//l6petame php laadimise
+				exit();
+				
+				
+				}
+
+
+				
+			}
+		} // login if end
+    // *********************
+    // ** LOO KASUTAJA *****
+    // *********************
+    if(isset($_POST["create"])){
+			if ( empty($_POST["create_email"]) ) {
+				$create_email_error = "See väli on kohustuslik";
+			}else{
+				$create_email = cleanInput($_POST["create_email"]);
+			}
+			if ( empty($_POST["create_password"]) ) {
+				$create_password_error = "See väli on kohustuslik";
+			} else {
+				if(strlen($_POST["create_password"]) < 8) {
+					$create_password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+				}else{
+					$create_password = cleanInput($_POST["create_password"]);
+				}
+			}
+			if(	$create_email_error == "" && $create_password_error == ""){
+				
+				
+				$password_hash = hash("sha512", $create_password);
+
+				
+				$create_response = $User->createUser($create_email, $password_hash);
+      }
+    } // create if end
+	}
+  // funktsioon, mis eemaldab kõikvõimaliku üleliigse tekstist
+  function cleanInput($data) {
+  	$data = trim($data); //tabulaator, tühikud, Enter
+  	$data = stripslashes($data); //Kaldkriipsud
+  	$data = htmlspecialchars($data); // 
+  	return $data;
+  }
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+
+  <h2>Log in</h2>
+  
+ <?php if(isset($kek_response->error)): ?>
+	<p style="color:red;"><?=$kek_response->error->message; ?>
+	</p>
+  <?php elseif(isset($kek_response->success)): ?>
+  	<p style="color:green;"><?=$kek_response->success->message; ?>
+	</p>
+  
+  <?php endif; ?>
+  
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+  	<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
+  	<input name="password" type="password" placeholder="Parool" value="<?php echo $password; ?>"> <?php echo $password_error; ?><br><br>
+  	<input type="submit" name="login" value="Log in">
+  </form>
+
+  <h2>Create user</h2>
+  <?php if(isset($create_response->error)): ?>
+	<p style="color:red;"><?=$create_response->error->message; ?>
+	</p>
+  <?php elseif(isset($create_response->success)): ?>
+  	<p style="color:green;"><?=$create_response->success->message; ?>
+	</p>
+  
+  <?php endif; ?>
+  
+  
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+  	<input name="create_email" type="email" placeholder="E-post" value="<?php echo $create_email; ?>"> <?php echo $create_email_error; ?><br><br>
+  	<input name="create_password" type="password" placeholder="Parool"> <?php echo $create_password_error; ?> <br><br>
+  	<input type="submit" name="create" value="Create user">
+  </form>
+<body>
+<html>
